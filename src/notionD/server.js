e82a06b2712931b053a49090c83bc33e1c582661
emedentsii <emedentsii@gmail.com>
2021-12-05 20:41:51 -0500

    added basic notion integration

--- src/notionD/server.js
+++ src/notionD/server.js
@@ -1,7 +1,6 @@
 import { Client } from "@notionhq/client"
 
-const NOTION_KEY = 'secret_qOzaJccdXooPxVnjwcS0tUkyjfFAeBFJe3ShJgcbS8S';
-const NOTION_DATABASE_ID = 'e0c0c05885f8465f83dec53702ef4fe9';
+
 const server = new Client({ auth: NOTION_KEY })
 
 const databaseId = NOTION_DATABASE_ID
